b5cb486899f41a5d0ca51329f07af8318da9c8b3
DMisztal <dominikmisztal@onet.eu>
2023-05-07 17:40:42 +0200

    Created website skeleton

--- myapp/app.js
+++ myapp/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var calendarRouter = require('./routes/calendar');
+var patientsRouter = require('./routes/patients');
+var visitsRouter = require('./routes/visits');
 
 var app = express();
 
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/calendar', calendarRouter);
+app.use('/visits', visitsRouter);
+app.use('/patients', patientsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- myapp/routes/patients.js
+++ myapp/routes/patients.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { pagetitle: 'Dentist+' });
+  res.render('patients', { pagetitle: 'Pacjenci' });
 });
 
 module.exports = router;

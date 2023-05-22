7bf04117c0e084094087ad971b5abab537d2fa79
Wanirur <235861@edu.p.lodz.pl>
2023-05-22 16:56:44 +0200

    add svgs to buttons

--- www/src/components/layout.tsx
+++ www/src/components/layout.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
+
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <div className="container">
       <header className="flex h-24 items-center gap-x-16 bg-gray-300 px-4">
         {" "}
-        {/* avatar */}
         <div className="h-16 w-16 rounded-full bg-red-700"></div>
         <div className="h-14 w-96 rounded-2xl bg-gray-400 p-3">
           {" "}
@@ -12,15 +13,39 @@ const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
         {/* empty div for creating gap */}
         <div className="w-96"></div>
         <div className="flex h-24 w-20 flex-col items-center pb-1">
-          <div className="h-[4.5rem] w-24 bg-green-500"></div>
+          <div className="flex h-[4.5rem] w-24 items-center justify-center bg-green-500">
+            {" "}
+            <Image
+              src={"chair.svg"}
+              alt="dentist chair"
+              width={60}
+              height={60}
+            ></Image>
+          </div>
           Wizyty
         </div>
         <div className="flex h-24 w-20 flex-col items-center pb-1">
-          <div className="h-[4.5rem] w-24 bg-green-500"></div>
+          <div className="flex h-[4.5rem] w-24 items-center justify-center bg-green-500">
+            {" "}
+            <Image
+              src={"patients.svg"}
+              alt="patients"
+              width={60}
+              height={60}
+            ></Image>
+          </div>
           Pacjenci
         </div>
         <div className="flex h-24 w-20 flex-col items-center pb-1">
-          <div className="h-[4.5rem] w-24 bg-green-500"></div>
+          <div className="flex h-[4.5rem] w-24 items-center justify-center bg-green-500">
+            {" "}
+            <Image
+              src={"calendar.svg"}
+              alt="calendar"
+              width={60}
+              height={60}
+            ></Image>
+          </div>
           Kalendarz
         </div>
       </header>

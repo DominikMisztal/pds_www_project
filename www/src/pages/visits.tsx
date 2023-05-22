abed0a120ebef9c314742150c17f8263c0b433fc
Wanirur <235861@edu.p.lodz.pl>
2023-05-22 20:05:44 +0200

    simplified visits page

--- www/src/pages/visits.tsx
+++ www/src/pages/visits.tsx
@@ -1,7 +1,8 @@
 import { type NextPage } from "next";
-import { randomPatients } from "~/utils";
+import Link from "next/link";
+import { randomVisits } from "~/utils";
 
-const Patients: NextPage = () => {
+const Visits: NextPage = () => {
   return (
     <div className="container h-[calc(100vh-6rem)]">
       <div className="h-16">
@@ -11,23 +12,28 @@ const Patients: NextPage = () => {
       </div>
       <div className="container flex h-[calc(100vh-10rem)] items-center justify-center bg-green-300">
         <div className="h-3/4 w-3/4 overflow-y-scroll bg-gray-200">
-          {randomPatients.map((patient) => {
+          {randomVisits.map((visit) => {
             return (
               <div
-                key={patient.index}
+                key={visit.index}
                 className="flex h-20 w-full items-center border border-solid border-black px-10"
               >
                 {" "}
-                <div className="w-1/2">
+                <div className="w-1/3"> {visit.date.toUTCString()}</div>
+                <div className="w-1/3">
                   {" "}
-                  {patient.name + " " + patient.surname}{" "}
+                  {visit.patient.name + " " + visit.patient.surname}{" "}
                 </div>
-                {/* empty div for gap */}
-                <div className="w-3/4"></div>
-                <button className="h-10 w-28 rounded-full bg-blue-400">
+                <div className="flex w-1/3 justify-end">
                   {" "}
-                  Wizyty{" "}
-                </button>
+                  <Link href="/visit_details">
+                    {" "}
+                    <button className="h-10 w-28 rounded-full bg-blue-400">
+                      {" "}
+                      Szczegóły{" "}
+                    </button>{" "}
+                  </Link>
+                </div>
               </div>
             );
           })}
@@ -37,4 +43,4 @@ const Patients: NextPage = () => {
   );
 };
 
-export default Patients;
+export default Visits;
---
+++ www/src/utils.ts
@@ -0,0 +1,29 @@
+export type Patient = {
+  index: number;
+  name: string;
+  surname: string;
+};
+
+export type Visit = {
+  index: number;
+  date: Date;
+  patient: Patient;
+};
+
+export const randomPatients = [
+  { index: 0, name: "Adam", surname: "Awokado" },
+  { index: 1, name: "Jan", surname: "Pietruszka" },
+  { index: 2, name: "Anna", surname: "Bakłażan" },
+  { index: 3, name: "Bartosz", surname: "Dynia" },
+  { index: 4, name: "Julia", surname: "Cebula" },
+  { index: 5, name: "Stefan", surname: "Papryka" },
+] as Patient[];
+
+export const randomVisits = [
+  { index: 0, date: new Date(2023, 3, 21, 19, 20), patient: randomPatients[0] },
+  { index: 1, date: new Date(2023, 4, 18, 19, 20), patient: randomPatients[1] },
+  { index: 2, date: new Date(2023, 3, 16, 19, 20), patient: randomPatients[2] },
+  { index: 3, date: new Date(2023, 2, 12, 19, 20), patient: randomPatients[3] },
+  { index: 4, date: new Date(2022, 6, 11, 19, 20), patient: randomPatients[4] },
+  { index: 5, date: new Date(2023, 7, 21, 19, 20), patient: randomPatients[5] },
+] as Visit[];

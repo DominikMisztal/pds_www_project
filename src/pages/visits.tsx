1095c3f5fa9b7a280d4aa3c1cf7ed3ea7127e1ca
Wanirur <235861@edu.p.lodz.pl>
2023-09-14 19:30:28 +0200

    fetch visits from db

--- src/pages/visits.tsx
+++ src/pages/visits.tsx
@@ -1,14 +1,70 @@
 import { Plus } from "lucide-react";
 import { type NextPage } from "next";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import Loading from "~/components/loading";
 import NewVisitForm from "~/components/new_visit";
-import { randomVisits } from "~/utils";
+import VisitsList from "~/components/visits";
+import { ITEMS_PER_PAGE, Visit } from "~/utils";
 
 const Visits: NextPage = () => {
   const [isUserAddingVisit, setIsUserAddingVisit] = useState<boolean>(false);
 
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [visits, setVisits] = useState<Visit[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isNextPageAvailable, setIsNextPageAvailable] = useState<boolean>(true);
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      setIsLoading(true);
+      console.log("inView");
+    }
+  }, [inView]);
+
+  useEffect(() => {
+    if (!isLoading || !isNextPageAvailable) {
+      return;
+    }
+
+    const controller = new AbortController();
+
+    const fetchUsers = async (page: number) => {
+      const res = await fetch(
+        `http://localhost:3001/database/my_visits?page=${page}`,
+        {
+          credentials: "include",
+          signal: controller.signal,
+        }
+      );
+
+      if (res.ok) {
+        const { data, meta } = (await res.json()) as {
+          data: Visit[];
+          meta: { page: string };
+        };
+
+        setIsLoading(false);
+        setPageNumber(+meta.page + 1);
+        setIsNextPageAvailable(data.length === ITEMS_PER_PAGE);
+        setVisits((old) =>
+          old
+            ? [...old.slice(0, (+meta.page - 1) * ITEMS_PER_PAGE), ...data]
+            : data
+        );
+      }
+    };
+
+    fetchUsers(pageNumber).catch((e) => {
+      console.error(e);
+    });
+
+    return () => controller.abort();
+  }, [isLoading, inView, pageNumber, isNextPageAvailable]);
+
   return (
     <>
       {isUserAddingVisit && (
@@ -34,30 +90,11 @@ const Visits: NextPage = () => {
         </button>
       </div>
       <div className="flex h-[calc(100vh-10rem)] w-full items-center justify-center bg-green-300">
-        <div className="h-3/4 w-3/4 overflow-y-scroll bg-gray-200">
-          {randomVisits.map((visit, index) => {
-            return (
-              <div
-                key={index}
-                className="flex h-20 w-full items-center border border-solid border-black px-10 text-xs lg:text-base"
-              >
-                <div className="w-1/3 p-3 text-center">
-                  {visit.date.toUTCString()}
-                </div>
-                {/* <div className="w-1/3 p-3 text-center">
-                  {visit.patientId + " " + visit.patient.surname}
-                </div> */}
-                <div className="flex w-1/3 justify-end">
-                  <Link href="/visit_details">
-                    <button className="h-8 w-16 rounded-full bg-blue-400 lg:h-10 lg:w-28">
-                      Szczegóły
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+        {isLoading ? (
+          <Loading></Loading>
+        ) : (
+          <VisitsList visits={visits}></VisitsList>
+        )}
       </div>
     </>
   );

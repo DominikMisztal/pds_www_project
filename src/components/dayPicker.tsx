fe6d3474135b353dc206c3cdca4b4d0854c97e32
Czapi12344 <236013@edu.p.lodz.pl>
2023-09-14 23:19:56 +0200

    calendar add year and month button

--- src/components/dayPicker.tsx
+++ src/components/dayPicker.tsx
@@ -1,67 +1,110 @@
 import React, { useState } from 'react';
 
+const DayPicker: React.FC<{
+  selectedDate: Date | undefined;
+  setDate: (selectedDate: Date) => void;
+}> = ({ selectedDate, setDate }) => {
+  const [currentMonth, setCurrentMonth] = useState<number>(
+    selectedDate ? selectedDate.getMonth() : new Date().getMonth()
+  );
+  const [currentYear, setCurrentYear] = useState<number>(
+    selectedDate ? selectedDate.getFullYear() : new Date().getFullYear()
+  );
 
- const DayPicker : React.FC<{ selectedDate:Date | undefined , setDate : (selectedDate : Date)=> void }> = ( { selectedDate , setDate}) => {
-        
-    
-    const handleDateClick = (date : Date) => {
-        setDate(date);
-    };
+  const handleDateClick = (date: Date) => {
+    setDate(date);
+  };
 
-    const renderCalendar = () => {
-        const currentDate = new Date();
-        const currentYear = currentDate.getFullYear();
-        const currentMonth = currentDate.getMonth();
-        const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-        const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
-      
-        const days = [];
-      
-        for (let i = 0; i < firstDayOfMonth; i++) {
-          days.push(<div key={`empty-${i}`} className="text-center"></div>);
-        }
-      
-    
-        for (let day = 1; day <= daysInMonth; day++) {
-          const date = new Date(currentYear, currentMonth, day);
-            const isSelected = selectedDate && selectedDate.toDateString() === date.toDateString();
-      
-          days.push(
-            <div
-              key={day}
-              onClick={() => handleDateClick(date)}
-              className={`text-center cursor-pointer p-2 ${
-                isSelected ? 'bg-green-500 text-white rounded-full' : ''
-              }`}
-            >
-              {day}
-            </div>
-          );
-        }
-      
-        return days;
-      };
+  const handlePrevMonth = () => {
+    const newMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+    const newYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+    setCurrentMonth(newMonth);
+    setCurrentYear(newYear);
+  };
 
+  const handleNextMonth = () => {
+    const newMonth = currentMonth === 11 ? 0 : currentMonth + 1;
+    const newYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+    setCurrentMonth(newMonth);
+    setCurrentYear(newYear);
+  };
+
+  const handlePrevYear = () => {
+    const newYear = currentYear - 1;
+    setCurrentYear(newYear);
+  };
+
+  const handleNextYear = () => {
+    const newYear = currentYear + 1;
+    setCurrentYear(newYear);
+  };
+
+  const renderCalendar = () => {
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+
+    const days = [];
+
+    for (let i = 0; i < firstDayOfMonth; i++) {
+      days.push(<div key={`empty-${i}`} className="text-center"></div>);
+    }
+
+    for (let day = 1; day <= daysInMonth; day++) {
+      const date = new Date(currentYear, currentMonth, day);
+      const isSelected =
+        selectedDate && selectedDate.toDateString() === date.toDateString();
+
+      days.push(
+        <div
+          key={day}
+          onClick={() => handleDateClick(date)}
+          className={`text-center cursor-pointer p-2 ${
+            isSelected ? 'bg-green-500 text-white rounded-full' : ''
+          }`}
+        >
+          {day}
+        </div>
+      );
+    }
+
+    return days;
+  };
 
   return (
     <div className="w-[90%] mx-auto mt-8 p-4 border rounded-lg shadow-lg bg-green-300 rounded-xl">
-      <h2 className="text-2xl font-semibold   ">Select a Date</h2>
-            <div className="grid grid-cols-7 gap-2 gap-y-11  bg-green-200 rounded-xl">
-                <div className="text-center">Sun</div>
-                <div className="text-center">Mon</div>
-                <div className="text-center">Tue</div>
-                <div className="text-center">Wed</div>
-                <div className="text-center">Thu</div>
-                <div className="text-center">Fri</div>
-                <div className="text-center">Sat</div>
+      <div className="flex justify-between mb-4">
+        <button onClick={handlePrevYear} className="text-xl cursor-pointer">
+          &#8592; Year
+        </button>
+        <button onClick={handlePrevMonth} className="text-xl cursor-pointer">
+          &#8592; Month
+        </button>
+        <h2 className="text-2xl font-semibold">
+          {new Date(currentYear, currentMonth).toLocaleString('default', {
+            month: 'long',
+            year: 'numeric',
+          })}
+        </h2>
+        <button onClick={handleNextMonth} className="text-xl cursor-pointer">
+          Month &#8594;
+        </button>
+        <button onClick={handleNextYear} className="text-xl cursor-pointer">
+          Year &#8594;
+        </button>
+      </div>
+      <div className="grid grid-cols-7 gap-2 gap-y-11  bg-green-200 rounded-xl">
+        <div className="text-center">Sun</div>
+        <div className="text-center">Mon</div>
+        <div className="text-center">Tue</div>
+        <div className="text-center">Wed</div>
+        <div className="text-center">Thu</div>
+        <div className="text-center">Fri</div>
+        <div className="text-center">Sat</div>
 
-                {renderCalendar()}
-
-
-            </div>
+        {renderCalendar()}
+      </div>
     </div>
   );
 };
-
 
 export default DayPicker;
--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,14 +1,62 @@
 import { type NextPage } from "next";
 
-import { format } from 'date-fns';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DayPicker from "~/components/dayPicker";
-import { randomVisits } from "~/utils";
+import { ITEMS_PER_PAGE, Visit } from "~/utils";
 
+import Loading from "~/components/loading";
 
 
 const CalendarPage: NextPage = () => {
   
+  const [isUserAddingVisit, setIsUserAddingVisit] = useState<boolean>(false);
+
+  const [visits, setVisits] = useState<Visit[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isNextPageAvailable, setIsNextPageAvailable] = useState<boolean>(true);
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  useEffect(() => {
+    if (!isLoading || !isNextPageAvailable) {
+      return;
+    }
+
+    const controller = new AbortController();
+
+    const fetchUsers = async (page: number) => {
+      const res = await fetch(
+        `http://localhost:3001/database/my_visits?page=${page}`,
+        {
+          credentials: "include",
+          signal: controller.signal,
+        }
+      );
+
+      if (res.ok) {
+        const { data, meta } = (await res.json()) as {
+          data: Visit[];
+          meta: { page: string };
+        };
+
+        setIsLoading(false);
+        setPageNumber(+meta.page + 1);
+        setIsNextPageAvailable(data.length === ITEMS_PER_PAGE);
+        setVisits((old) =>
+          old
+            ? [...old.slice(0, (+meta.page - 1) * ITEMS_PER_PAGE), ...data]
+            : data
+        );
+      }
+    };
+
+    fetchUsers(pageNumber).catch((e) => {
+      console.error(e);
+    });
+
+    return () => controller.abort();
+  }, [isLoading,  pageNumber, isNextPageAvailable]);
+
+
   const [selected, setSelected] = useState<Date>( );
 
   return  <div className="flex h-[calc(100vh-10rem)] w-full items-center justify-center bg-green-500">
@@ -23,25 +71,27 @@ const CalendarPage: NextPage = () => {
 
       </div>
       <div className="flex-1 h-full w-full ">
-        <div className="w-[90%] mx-auto mt-8 p-4 border rounded-lg shadow-lg bg-green-300 rounded-xl">
+        <div className="w-[90%] h-3/4 mx-auto mt-8 p-4 border rounded-lg shadow-lg bg-green-300 rounded-xl">
             <div className="w-[100%] h-[100%] bg-green-200 rounded-xl">
               <div className="h-[100%] w-[100%] overflow-y-scroll bg-green-200">
-                  {randomVisits.map((visit) => {
-                    return (
-                      <div
-                        key={visit.index}
-                        className="flex h-20 w-full items-center border border-solid border-green-300 px-10 text-xs lg:text-base"
-                      >
-                        <div className="w-1/3 p-3 text-center">
-                          {visit.date.toUTCString()}
-                        </div>
-                        <div className="w-1/3 p-3 text-center">
-                          {visit.patient.name + " " + visit.patient.surname}
-                        </div>
-                      
+                  { visits ? visits.map((visit, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="flex h-20 w-full items-center border border-solid border-black px-10 text-xs lg:text-base"
+                    >
+                      <div className="w-1/2 p-3 text-center">
+                        {visit.date.replace(/[TZ]/g, " ")}
                       </div>
-                    );
-                  })}
+                      <div className="w-1/2 p-3 text-center">
+                        {visit.name + " " + visit.surname}
+                      </div>
+                    
+                    </div>
+                  );
+                }) : <Loading/>
+                                
+                }
               </div>
             </div>
         </div>

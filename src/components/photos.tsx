825716c1ee54baa63f0a0ea34647b2c50bdea133
Czapi12344 <236013@edu.p.lodz.pl>
2023-09-14 16:06:31 +0200

    Added enlargedPhoto

--- src/components/photos.tsx
+++ src/components/photos.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { SetStateAction, useCallback, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { Loader, Plus } from "lucide-react";
 import { useDropzone } from "react-dropzone";
@@ -12,9 +12,19 @@ const Photos: React.FC<{ visitId: number }> = ({ visitId }) => {
   const [photos, setPhotos] = useState<string[]>([]);
   const [photosReadyToUpload, setPhotosReadyToUpload] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-
   //react runs useEffect twice in dev so this ref prevents fetching photos twice
   const fetchedRef = useRef<boolean>(false);
+
+
+  const [enlargedPhoto, setEnlargedPhoto] = useState<string>(''); 
+
+  const openEnlargedPhoto = (photoUrl: string) => {
+    setEnlargedPhoto(photoUrl);
+  };
+
+  const closeEnlargedPhoto = () => {
+    setEnlargedPhoto(''); // Change null to ''
+  };
 
   useEffect(() => {
     if (fetchedRef.current) {
@@ -69,22 +79,23 @@ const Photos: React.FC<{ visitId: number }> = ({ visitId }) => {
   );
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
-
+  
   if (isLoading) {
     return (
-      <div className="flex h-full w-full items-center justify-center gap-2 text-xl">
+      <div className="flex h-full w-full items-center justify-center gap-2 text-xl bg-green-300">
         <Loader className="animate-spin"></Loader>
         Loading photos...
       </div>
     );
   }
   return (
-    <div className="grid h-full w-full auto-cols-max auto-rows-max grid-cols-1 gap-y-2 overflow-y-scroll p-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3">
+    <div className="grid h-full w-full auto-cols-max auto-rows-max grid-cols-1 gap-y-2 gap-x-5 overflow-y-scroll p-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3  ">
       {photos.map((item) => {
         return (
           <div
             key={item}
-            className="relative  h-24 w-48 rounded-xl border-4 border-blue-400 p-3"
+            className="relative h-33 w-33 rounded-xl border-5 border-blue-400 p-3 aspect-ratio-1/1"
+            onClick={() => openEnlargedPhoto(item)}
           >
             <Image src={item} fill alt="photo" className="rounded-xl"></Image>
           </div>
@@ -95,16 +106,26 @@ const Photos: React.FC<{ visitId: number }> = ({ visitId }) => {
         return (
           <div
             key={item}
-            className="relative  h-24 w-48 rounded-xl border-4 border-blue-400 p-3"
+            className="relative  h-33 w-33 rounded-xl border-4 border-blue-400 p-3 aspect-ratio-1/1"
+            onClick={() => openEnlargedPhoto(item)}
           >
-            <Image src={item} fill alt="photo" className="rounded-xl"></Image>
+            <Image src={item} fill alt="photo" ></Image>
           </div>
         );
       })}
 
+
+      {enlargedPhoto && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50" onClick={closeEnlargedPhoto}>
+          <div className="max-w-4xl bg-white p-4 rounded-lg">
+            <img src={enlargedPhoto} alt="enlarged-photo"    />
+          </div>
+        </div>
+      )}
+    
       <div
         {...getRootProps()}
-        className="flex h-24 w-48 border-spacing-4 cursor-pointer flex-col items-center justify-center gap-1 rounded-xl border-4 border-dashed border-blue-400 p-3 hover:bg-gray-100"
+        className="flex h-24 w-33 border-spacing-4 cursor-pointer flex-col items-center justify-center gap-1 rounded-xl border-4 border-dashed border-blue-400 p-3 hover:bg-gray-100"
       >
         <input
           type="file"
@@ -124,5 +145,6 @@ const Photos: React.FC<{ visitId: number }> = ({ visitId }) => {
     </div>
   );
 };
+
 
 export default Photos;
